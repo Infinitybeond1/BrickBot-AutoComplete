9cbe37cfc1a8208d00493e7325e2a9e49c11c64d
Infinitybeond1 <aksharvadaria9@gmail.com>
2021-11-07 17:16:38 +0000

    fix invite command

--- Commands/Information/invite.js
+++ Commands/Information/invite.js
@@ -12,10 +12,12 @@ module.exports = new Command({
     // command start
     run: async ({ client, interaction, args }) => {
         let embed = new MessageEmbed()
-            .setColor(ee.embed_color)
-            .setTitle(`Thanks For Inviting Me.`)
-            .setDescription(`>>> [Click to Invite](https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
-            .setFooter(ee.embed_footertext, ee.embed_footericon)
+          .setColor(ee.embed_color)
+          .setTitle(`Click the link below to invite me`)
+          .setDescription(
+            `>>> [Click to Invite](https://discord.com/api/oauth2/authorize?client_id=875148976272343041&permissions=8&redirect_uri=https%3A%2F%2Fdiscord.events.stdlib.com%2Fdiscord%2Fauth%2F&scope=applications.commands%20bot)`
+          )
+          .setFooter(ee.embed_footertext, ee.embed_footericon);
         interaction.followUp({ embeds: [embed], ephemeral: true })
     }
 })
