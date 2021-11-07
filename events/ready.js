79d20e6db5559ece3b8befe5ad7b8da41bd5b72c
Infinitybeond1 <aksharvadaria9@gmail.com>
2021-11-07 16:38:54 +0000

    fix presence

--- events/ready.js
+++ events/ready.js
@@ -2,5 +2,5 @@ const client = require("..");
 
 client.on('ready', () => {
     console.log(`${client.user.username} Is Online`);
-    client.user.setActivity(`Kabir Coding me`,{type : "STREAMING", url : "https://twitch.com/tbg"});
+    client.user.setActivity(`Hello World`,{type : "PLAYING"});
 })
